c272014c6a108d4c0d301a0a453dbf915009abf2
amench30 <mencher@gmail.com>
2015-01-10 23:37:30 -0800

    added another function

--- javascrip_basic.js
+++ javascrip_basic.js
@@ -147,3 +147,36 @@ if (correct === 5) {
 }
 
 
+//Learning functions
+
+function isEmailEmpty() {
+  var field = document.getElementById(‘email’);
+  if (field.value === ‘’) {
+    return true;
+  }  else {
+      return false;
+  }
+}
+
+var fieldTest = isEmailEmpty();
+if (fieldTest === true) {
+    alert(‘Please provide your email address.’);
+}
+
+function getRandomNumber( upper ) {
+  var randomNumber = Math.floor( Math.random() * upper ) + 1; 
+  return randomNumber;
+}
+/*
+console.log( getRandomNumber(6) );
+console.log( getRandomNumber(100) );
+console.log( getRandomNumber(1000) );
+console.log( getRandomNumber(2) );
+*/
+
+function getArea(width, length, unit) {
+  var area = width * length;
+  return area + “ “ + unit;
+}
+console.log(getArea(10, 20, ’sq ft’));
+
